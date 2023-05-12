3bae2caa09470238d83e39a53c6153f22a695fa2
Norbert Frank Mba <norbertmbafrank@gmail.com>
2023-05-12 17:43:55 +0100

    governance token base

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,26 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.0",
+      },
+      {
+        version: "0.8.9",
+      },
+    ],
+  },
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/YOUR_INFURA_PROJECT_ID",
+      // accounts: [`0x${YOUR_PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: "YOUR_ETHERSCAN_API_KEY",
+  },
 };
 
 export default config;
