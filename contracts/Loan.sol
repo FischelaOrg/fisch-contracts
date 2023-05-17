548fb4a20626b65d13cccc12911f3dd003dae247
Norbert Frank Mba <norbertmbafrank@gmail.com>
2023-05-17 17:04:51 +0100

    loan contract blueprint

--- contracts/Loan.sol
+++ contracts/Loan.sol
@@ -4,16 +4,88 @@ pragma solidity ^0.8.8;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Loan is Ownable {
-    uint256 private value;
+    // default loan duration
+    uint256 private _defaultLoanDuration = 12 weeks;
+    uint256 public defaultInterestRate = 5;
 
-    event ValueChanged(uint256 newValue);
+    // create a struct for Lenders
+    struct Lender{
+        uint256 loanId;
+        uint256 innitialBorrowAmount;
+        uint256 currentBorrowAmount;
+        uint256 amountRepaid;
+        uint256 borrowerId;
+        uint256 interestRate; // using compound interest SI = PRT/100 to calculate interest rate
+        uint256 loanOutDuration;
+        bool locked;
+        bool isActive;
+        address lender;
 
-    function store(uint256 newValue) public onlyOwner {
-        value = newValue;
-        emit ValueChanged(newValue);
     }
 
-    function retrieve() public view returns (uint256) {
-        return value;
+    event LoanCreated(
+
+    );
+
+    event LoanBorrowed(
+
+    );
+
+    // create a struct for borowers
+    struct Borrower{
+        uint256 borrowerId;
+        address borrower;
+        uint256 borrowAmount;
+        uint256 repaymentCapacity;
+        uint256 deadline;
+        uint256 interest;
+        uint256 lenderId;
+        uint256 nftCollateralId
+
+    }
+
+    // create a list of lenders
+
+    Lender[] private _lenders;
+
+    // create a list of borrrowers
+
+    Borrower[] private _borrowers;
+
+    // createLoan
+    function createLoan(uint256 innitialBorrowAmount, uint256 interestRate, uint256 loanOutDuration) public payable{
+        // create Loan
+    }
+
+    // calculate simple interest
+    function calculateLoanInterest() public {
+
+    }
+
+    // first create a function to borrow all
+    // Borrow
+
+    function borrow() public {
+
+    }
+
+    // repayLoan ---- monthly repayment of loans
+    function repayLoan() public {
+
+    }
+
+    // liquidateCollateral
+    function liquidateCollateral() public {
+
+    }
+
+    // approveLoan
+    function approveLoan() public {
+
+    }
+
+    // cancelLoan
+    function cancelLoan() public {
+
     }
 }
